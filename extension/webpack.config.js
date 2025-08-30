ebd426b5fdc846e8070fb6e323f23b2dc2b1aa5e
Khongchai <world1955@hotmail.com>
2025-08-30 13:30:28 +0700

    removed minification

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -13,6 +13,9 @@ const extensionConfig = {
 	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
   entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  optimization: {
+    minimize: false,
+  },
   output: {
     // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
     path: path.resolve(__dirname, 'dist'),
@@ -40,7 +43,7 @@ const extensionConfig = {
       }
     ]
   },
-  devtool: 'nosources-source-map',
+  devtool: 'source-map',
   infrastructureLogging: {
     level: "log", // enables logging required for problem matchers
   },
