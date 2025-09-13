feabbc2bdf70a67376d1ce01c85994a9a4b7a288
Khongchai <world1955@hotmail.com>
2025-09-13 13:47:34 +0700

    no need for loop construct

--- vocab-ls/vocabulary/scanner.go
+++ vocab-ls/vocabulary/scanner.go
@@ -20,75 +20,75 @@ func NewScanner(text string) *Scanner {
 }
 
 func (s *Scanner) Scan() (Token, string) {
-	for {
-		if s.atEnd() {
-			return TokenEOF, ""
-		}
+	if s.atEnd() {
+		return TokenEOF, ""
+	}
 
-		if lib.IsLineBreak(s.char()) {
-			return TokenLineBreak, ""
-		}
+	if lib.IsLineBreak(s.char()) {
+		return TokenLineBreak, ""
+	}
 
-		// Date literal
-		if lib.IsDigit(s.char()) {
-			collected := ""
-			for i := range syntax.DateLength {
-				collected += string(s.char())
+	// Date literal
+	if lib.IsDigit(s.char()) {
+		collected := ""
+		for i := range syntax.DateLength {
+			collected += string(s.char())
 
-				// invalid cases
-				if (i == syntax.DateSlashFirstPosition || i == syntax.DateSlashSecondPostiion) && s.char() != lib.Slash {
-					return TokenText, collected
-				} else if !lib.IsDigit(s.char()) {
-					return TokenText, collected
-				}
-
-				s.pos++
-				if s.atEnd() {
-					break
-				}
-			}
-
-			s.pos += syntax.DateLength
-			return TokenDateLiteral, collected
-		}
-
-		// try parse language ident
-		if lib.LeftParen == s.char() {
-			collected := ""
-
-			for range syntax.LanguageIdentifierLength - 2 { // minus left and right parent
-				collected += string(s.char())
-
-				if !lib.IsASCIILetter(s.char()) {
-					return TokenText, collected
-				}
-
-				s.pos++
-				if s.atEnd() {
-					break
-				}
-			}
-
-			if s.char() == lib.RightParen {
-				s.pos++ // If it's not right parent then we don't care / don't read and therefore not forwarding it.
-				return TokenLanguageIdent, collected
-			} else {
+			// invalid cases
+			if (i == syntax.DateSlashFirstPosition || i == syntax.DateSlashSecondPostiion) && s.char() != lib.Slash {
+				return TokenText, collected
+			} else if !lib.IsDigit(s.char()) {
 				return TokenText, collected
 			}
 
+			s.pos++
+			if s.atEnd() {
+				break
+			}
 		}
 
-		if lib.LessThan == s.char() {
-			// try parse <!-- end section -->
-			// comment
-		}
-
-		if lib.GreaterThan == s.char() {
-			// try parse new vocab and reviewed vocab section
-		}
-
-		return TokenText, string(s.char())
+		s.pos += syntax.DateLength
+		return TokenDateLiteral, collected
 	}
+
+	// try parse language ident
+	if lib.LeftParen == s.char() {
+		collected := ""
+
+		for range syntax.LanguageIdentifierLength - 2 { // minus left and right parent
+			collected += string(s.char())
+
+			if !lib.IsASCIILetter(s.char()) {
+				return TokenText, collected
+			}
+
+			s.pos++
+			if s.atEnd() {
+				break
+			}
+		}
+
+		if s.char() == lib.RightParen {
+			s.pos++ // If it's not right parent then we don't care / don't read and therefore not forwarding it.
+			return TokenLanguageIdent, collected
+		} else {
+			return TokenText, collected
+		}
+
+	}
+
+	if lib.LessThan == s.char() {
+		// try parse <!-- end section -->
+		// comment
+		return TokenText, ""
+	}
+
+	if lib.GreaterThan == s.char() {
+		// try parse new vocab and reviewed vocab section
+		return TokenText, ""
+	}
+
+	return TokenText, string(s.char())
 
 }
 
