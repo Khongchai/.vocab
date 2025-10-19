195f53d90f5cfd1bb8c51315f9148195e4149fb3
khongchai <world1955@hotmail.com>
2025-10-19 14:30:53 +0700

    feeling lazy

--- vocab-ls/vocabulary/compiler/compiler.test.go
+++ vocab-ls/vocabulary/compiler/compiler.test.go
@@ -2,17 +2,12 @@ package compiler
 
 import (
 	"testing"
-	test "vocab/vocab_testing"
-	"vocab/vocabulary/parser"
 )
 
-func TestAstToWordTree(t *testing.T) {
-	text := test.TrimLines(`
-		20/08/2025	
-		> (it) sfatare, il mito
-		Non per sfatare il mito della città eterna, della Roma magica che vedete anche nei film, soprattutto nei film di Fellini, ma per mostrarvi la verità o comunque la vera realtà di Roma e dei romani. 
-	`)
-	parser := parser.NewParser(t.Context(), "xxx", parser.NewScanner(text), func(a any) {})
-	parser.Parse()
+func FakeTree() {
 
+}
+
+func TestCompilerDiagnosticsMapping_GivenExistingDiagnosticsFromTree_ShouldAlsoAddToFinalDiagnostics(t *testing.T) {
+	// Too lazy, do later.
 }
