da4f66069099d3b0752a18fe47d7e0231c66976d
khongchai <world1955@hotmail.com>
2025-10-23 13:16:14 +0700

    removed never seen before warning

--- vocab-ls/vocabulary/compiler/helper.go
+++ vocab-ls/vocabulary/compiler/helper.go
@@ -1,7 +1,6 @@
 package compiler
 
 import (
-	"fmt"
 	lsproto "vocab/lsp"
 	"vocab/vocabulary/parser"
 )
@@ -13,32 +12,8 @@ func AstToWordTree(ast *parser.VocabAst) *WordTree {
 		allWordSections := append(section.NewWords, section.ReviewedWords...)
 		for _, wordSection := range allWordSections {
 			lang := wordSection.Language
-			reviewed := wordSection.Reviewed
 			for _, word := range wordSection.Words {
-				var diag []*lsproto.Diagnostic
-				existingTwigs := tree.GetTwigs(lang, word.Text)
-
-				warnText := func() string {
-					if !reviewed && len(existingTwigs) != 0 {
-						return fmt.Sprintf("This has been seen before. First occurence in %s", existingTwigs[0].GetLocation())
-					} else if reviewed && len(existingTwigs) == 0 {
-						return "This has never been seen before"
-					}
-					return ""
-				}()
-
-				if warnText != "" {
-					warn := lsproto.MakeDiagnostics(
-						warnText,
-						wordSection.Line,
-						word.Start,
-						word.End,
-						lsproto.DiagnosticsSeverityWarning,
-					)
-					diag = append(diag, warn)
-				}
-
-				tree.AddTwig(lang, word, ast.Uri, section, diag)
+				tree.AddTwig(lang, word, ast.Uri, section, []*lsproto.Diagnostic{})
 			}
 		}
 	}
